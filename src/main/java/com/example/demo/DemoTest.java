f3d9b321d19e1e88ed253891a3fcd4d055a1fe16
Mujaheed127 <mujaheed.jaffar@hcltech.com>
2025-10-22 15:41:50 +0530

    Update DemoTest.java

--- src/main/java/com/example/demo/DemoTest.java
+++ src/main/java/com/example/demo/DemoTest.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class DemoTest {
     public static void main(String[] args) {
         // Simulated real secret
-        String awsSecretKey = "AKIAIOSFODNN7EXAMPLE";
+        String awsSecretKey = "AKIA1234567890ABCDE";
         if (awsSecretKey != null)
             System.out.println("Using secret: " + awsSecretKey); // Missing braces (Checkstyle violation)
     }
